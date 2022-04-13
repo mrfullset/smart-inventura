dbf90ad18050aec2345a26b67753212decc3f3bc
Alexandr Goncharov <alex@yolkstudio.com>
2022-04-13 13:22:34 +0200

    fix index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,63 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import NumberSelector from "../components/NumberSelector";
-import styles from "../styles/Home.module.css";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import styles from "./index.module.scss";
+import { Button, Col, Row, Container, Form } from "react-bootstrap";
+import InputWithButton from "../components/InputWithButton";
+import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const [value, setValue] = useState(0);
+  const [counter, setCounter] = useState(0);
+  let [warehouses, setWarehouses] = useState<any[]>([]);
+  let [selectedWarehouse, setSelectedWarehouse] = useState({});
 
-  const onNumberChange = (value: number) => {
-    if (value < 0) {
-      return;
-    }
-    setValue(value);
-  };
+  useEffect(() => {
+    let t = "t";
+    let url = "https://inventura.flexibee.eu/v2/c/firma4/sklad?detail=full";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: "Basic YWRtaW40OmFkbWluNGFkbWluNA==",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setWarehouses(result.winstrom.sklad);
+
+          console.log(warehouses.length);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
 
   return (
-    <div className={styles.container}>
-      <NumberSelector value={value} onChange={onNumberChange} />
-    </div>
+    <Container className={styles.sklad}>
+      <div className={styles.pageContent}>
+        <Row>
+          <h1 className={styles.name + " display-1"}>KAPYBARA S.R.O</h1>
+        </Row>
+
+        <Row>
+          <div className={styles.warehouses}>
+            <div className={styles.warehouse}>Scan warehouse ID 🏭</div>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <InputWithButton buttonText="OK" placeholder="TEST ID" />
+                {/* <Form.Control type="email" className={styles.warehouseInput} placeholder="" /> */}
+              </Form.Group>
+            </Form>
+            <div className={styles.warehouse}>or choose it</div>
+            <div className={styles.warehouseList}>
+              {warehouses.map((wh) => (
+                <div className={styles.warehouseItem}> {wh.nazev} </div>
+              ))}
+            </div>
+          </div>
+        </Row>
+      </div>
+    </Container>
   );
 };
 
